5e7210b1e5358495b377661519c68a64080c2a9c
sulta <sulta@DESKTOP-E785JP2>
2023-09-27 14:45:53 +0300

    Добавьте файлы проекта.

---
+++ dz7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace dz7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 3 числа");
+            int firstNumber = int.Parse(Console.ReadLine());
+            int twoNumber = int.Parse(Console.ReadLine());
+            int treeNumber = int.Parse(Console.ReadLine());
+            int MaxValue; 
+            if ((firstNumber < twoNumber) && (twoNumber < firstNumber))
+            {
+                MaxValue = firstNumber;
+            }
+            else
+            {
+                if (twoNumber < firstNumber)
+                {
+                    MaxValue = twoNumber;
+                }
+                else
+                    MaxValue = treeNumber;
+            }
+            Console.WriteLine($"Минимальное значение = {MaxValue}");
+            Console.ReadKey();
+
+
+        }
+    }
+}
